861ea2064ede3513116cee513fe2e25b22b53499
annastriw <129259825+annastriw@users.noreply.github.com>
2025-05-28 20:35:06 +0700

    Add files via upload

---
+++ TA_TKO_FUNGSIALIH.m
@@ -0,0 +1,28 @@
+clear
+clc
+
+% Parameter sistem
+Ra = 0.4;
+La = 2.7;
+J = 0.0004;
+B = 0.0022;
+K = 0.015;
+Kb = 0.05;
+
+% Numerator dan Denominator transfer function
+num = K; 
+den = [La * J, (La * B + Ra * J), (Ra * B + Kb * K)];
+
+% Transfer Function
+Gs = tf(num, den);
+
+Kp = 0.31199;
+Ti = 3.4912;
+Td = 0.20971;
+Ki = Kp / Ti;
+Kd = Kp * Td;
+
+% Print nilai Kp, Ki, Kd
+fprintf('Nilai Kp = %.5f\n', Kp);
+fprintf('Nilai Ki = %.5f\n', Ki);
+fprintf('Nilai Kd = %.5f\n', Kd);
